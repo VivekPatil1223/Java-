2ac695e85be89813851616c5ab1f6b64c4cc8882
VivekPatil1223 <71131757+VivekPatil1223@users.noreply.github.com>
2020-11-05 15:30:34 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,9 @@
+public class addition
+{
+    public static void main (String args[])
+    {
+        int a=14,b=56,add;
+        add=a+b;
+        System.out.println("Sum of two nos:"+add);
+    }
+}
